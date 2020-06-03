cbd131e12624d298e0e864f4e78831ea2a9a703d
JakeDuke <36011510+JakeDuke@users.noreply.github.com>
2020-06-03 19:11:50 +0200

    table styles added

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -19,6 +19,10 @@ html {
   box-sizing: border-box;
 }
 
+body {
+  background-color: ghostwhite;
+}
+
 *,
 *:before,
 *:after {
@@ -26,32 +30,15 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
+table {
+  background-color: white;
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
+  th > * {
+    text-align: center;
+  }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+  td {
+    text-align: center;
+  }
 }
 </style>
